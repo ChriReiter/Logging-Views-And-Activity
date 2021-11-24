0a6594a63a22951cf27b75d521a2f79378760937
Christian Reiter <christian.reiter3@edu.fh-joanneum.at>
2021-11-24 20:35:24 +0100

    Rating and feedback now displayed in respective textView's

--- app/src/main/java/at/fh/mappdev/loggingviewsandactivity/RatingActivity.kt
+++ app/src/main/java/at/fh/mappdev/loggingviewsandactivity/RatingActivity.kt
@@ -1,12 +1,16 @@
 package at.fh.mappdev.loggingviewsandactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.RatingBar
+import android.widget.TextView
 
 class RatingActivity : AppCompatActivity() {
+
     private val rating = Rating("android_lesson_01")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,10 @@ class RatingActivity : AppCompatActivity() {
         rating.feedback = findViewById<EditText>(R.id.feedbackInput).text.toString()
         rating.rating = findViewById<RatingBar>(R.id.ratingBar).rating.toDouble()
 
+        val feedbackTxt:TextView = findViewById(R.id.feedback)
+        val ratingTxt:TextView = findViewById(R.id.rating_value)
 
+        feedbackTxt.text = rating.feedback.toString()
+        ratingTxt.text = rating.rating.toString()
     }
 }
